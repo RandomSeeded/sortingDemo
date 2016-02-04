654d78c54c74bec298380b2ab5f1300197b74c1b
Nate Willard <nate.a.willard@gmail.com>
2016-02-04 11:25:52 -0800

    Add best case to bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,12 +2,17 @@
 var bubbleSort = module.exports = function(arr) {
   return new Promise(function(resolve, reject) {
     for (var i = 0; i < arr.length; i++) {
+      var alreadySorted = true;
       for (var j = arr.length - 1; j > i; j--) {
         if (arr[j] < arr[j - 1]) {
           var temp = arr[j];
           arr[j] = arr[j - 1];
           arr[j - 1] = temp;
+          alreadySorted = false;
         }
+      }
+      if (alreadySorted) {
+        resolve(arr);
       }
     }
     resolve(arr);
